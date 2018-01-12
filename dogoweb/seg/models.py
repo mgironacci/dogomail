4a7d38ad136fca0e557ce3725e816eb2b90a5c9f
Andres Gironacci <mgironacci@itecnis.com>
2018-01-12 20:32:40 -0300

    [NEW] Cambio color de fondo, icono desde gravatar

--- dogoweb/seg/models.py
+++ dogoweb/seg/models.py
@@ -2,6 +2,10 @@
 from django.db import models
 from django.contrib.auth.models import User, Permission
 from dogoweb.settings import VERSION
+from django.utils import timezone
+import hashlib
+import datetime
+import urllib.request
 
 # Modelo de visualizacion ----------------------------------------------
 
@@ -10,7 +14,6 @@ class Control(models.Model):
     nombre = models.CharField(max_length=50)
     permiso = models.ForeignKey(Permission, on_delete=models.PROTECT)
     activo = models.BooleanField(default=True)
-
 
     def __repr__(self):
         return '<Control: nombre="%s">' % self.nombre
@@ -45,7 +48,7 @@ class Control(models.Model):
 
 
 class Menu(models.Model):
-    #objects=MenuManager()
+    # objects=MenuManager()
 
     idm = models.CharField(max_length=50)
     nombre = models.CharField(max_length=50, unique=True)
@@ -62,30 +65,30 @@ class Menu(models.Model):
     def __str__(self):
         return self.nombre
 
+        # class Meta:
+        #		permissions = (
+        #			("can_login", "Can login"),
+        #		)
 
-    # class Meta:
-    #		permissions = (
-    #			("can_login", "Can login"),
-    #		)
+        # class PantallaManager(models.Manager):
+        # 	def mi_select(self, yo, selmenu=None):
+        # 		if selmenu is None:
+        # 			return []
+        # 		mi_perms=yo.get_all_permissions()
+        # 		pants=[]
+        # 		m=Menu.objects.get(idm=selmenu)
+        # 		for pp in Pantalla.objects.filter(menu=m).filter(activo=True).order_by('orden'):
+        # 			if pp.permiso is None:
+        # 				pants.append(pp)
+        # 				continue
+        # 			mp=pp.permiso.content_type.app_label+'.'+pp.permiso.codename
+        # 			if mp in mi_perms:
+        # 				pants.append(pp)
+        # 		return pants
 
-    # class PantallaManager(models.Manager):
-    # 	def mi_select(self, yo, selmenu=None):
-    # 		if selmenu is None:
-    # 			return []
-    # 		mi_perms=yo.get_all_permissions()
-    # 		pants=[]
-    # 		m=Menu.objects.get(idm=selmenu)
-    # 		for pp in Pantalla.objects.filter(menu=m).filter(activo=True).order_by('orden'):
-    # 			if pp.permiso is None:
-    # 				pants.append(pp)
-    # 				continue
-    # 			mp=pp.permiso.content_type.app_label+'.'+pp.permiso.codename
-    # 			if mp in mi_perms:
-    # 				pants.append(pp)
-    # 		return pants
 
 class Pantalla(models.Model):
-    #objects = PantallaManager()
+    # objects = PantallaManager()
 
     idm = models.CharField(max_length=50)
     nombre = models.CharField(max_length=50)
@@ -105,27 +108,51 @@ class Pantalla(models.Model):
     def __str__(self):
         return self.nombre
 
+
 # Perfil de usuario ----------------------------------------------------
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gravatar = models.CharField(max_length=32, default=None)
+    last_gravatar = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.username
 
-    def dogoversion(self):
+    def version(self):
         return VERSION
 
     def mis_menus(self):
         ret = []
         if self.user.is_superuser:
-            menus = Menu.objects.filter(activo = True)
+            menus = Menu.objects.filter(activo=True)
         else:
-            menus = Menu.objects.filter(activo = True)
+            # TODO: usar permisos de usuarios para obtener pantallas
+            menus = Menu.objects.filter(activo=True)
         for m in menus:
             ret.append({
                 'nombre': m.nombre,
                 'icono': m.icono,
-                'pantallas': m.pantalla_set.filter(activo = True),
+                'pantallas': m.pantalla_set.filter(activo=True),
             })
+        return ret
+
+    def mi_avatar(self):
+        if self.user.is_superuser:
+            return "/static/dogoweb/img/dogoadmin.png"
+        elif self.user.is_staff:
+            ret = "/static/dogoweb/img/dogostaff.png"
+        else:
+            ret = "/static/dogoweb/img/dogouser.png"
+        # Reviso si hay gravatar
+        grahash = hashlib.md5(self.user.username.lower().encode("utf8")).hexdigest()
+        if (self.last_gravatar < timezone.now() - datetime.timedelta(minutes=10)
+            or self.gravatar is None):
+            try:
+                graurl = "https://www.gravatar.com/avatar/"+grahash
+                urllib.request.urlopen("https://www.gravatar.com/avatar/"+grahash+"?d=404")
+                ret = graurl
+            except:
+                self.last_gravatar = timezone.now()
+
         return ret
